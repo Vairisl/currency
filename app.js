0f1e93f99142d5f27fefa80c6b9e4dcfc19930f1
Vairis <vairiks123@gmail.com>
2016-11-03 20:20:28 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let x = 5
+let y = 10
+
+let min = function(x, y) {
+    if(x > y){
+        return y
+    } else {
+        return x
+    }
+} 
+//---------------------------------------------------------------
+let c = min(5, 6)
+let z = min(12, 8)
+let f = min(-4, -5)
