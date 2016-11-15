eafd23b18113a848efd13a1fe181d9c1a26316c1
Vairis <vairiks123@gmail.com>
2016-11-15 20:37:32 +0200

    MD1 alternativa

---
+++ md1alt.js
@@ -0,0 +1,25 @@
+let grid = []
+grid[0] = [3,3,3,3,null,1,1,1]
+grid[1] = [null,3,3,3,6,1,1,1]
+grid[2] = [null,null,3,2,2,2,2,null]
+grid[3] = [null,null,4,2,4,5,null,null]
+grid[4] = [null,null,4,4,4,null,6,6]
+grid[5] = [null,null,4,4,4,null,6,6]
+grid[6] = [null,null,4,4,4,null,6,6]
+grid[7] = [7,null,null,null,null,null,6,6]
+grid[8] = [7,7,null,null,null,1,1,1]
+grid[9] = [7,7,7,null,null,1,1,1]
+
+
+function getValue(y, x, array){
+    if(typeof array[x][y] === 'undefined') {
+        console.log (Lol)
+}
+    else {
+       console.log (array[x][y])
+}
+}
+
+getValue(0,0,grid);
+
+
