402c5b0416ba3f404453f98c2f4d31068dbd57ec
Vairis <vairiks123@gmail.com>
2016-11-15 19:11:59 +0200

    Klases darbs #1

---
+++ calculate.js
@@ -0,0 +1,14 @@
+let calculate = function(price, tax){
+    return price - (price * tax)
+}
+
+let pvn = 0.21
+let income = 1025.57
+
+let money = calculate(income, pvn)
+
+let outcome = 22.5
+let earned = money - outcome
+
+
+
