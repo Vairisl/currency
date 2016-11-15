6b32bf9656636393f5c9db4a40a2c0d03557445f
Vairis <vairiks123@gmail.com>
2016-11-15 19:12:19 +0200

    Majas darbs #1

---
+++ md1.js
@@ -0,0 +1,168 @@
+let Coords = function(x, y) {
+    if (x == 1 && y == 1) {
+        atbilde = 3
+    } else if (x == 2 && y == 1) {
+        atbilde = 3
+    } else if (x == 3 && y == 1) {
+        atbilde = 3
+    } else if (x == 4 && y == 1) {
+        atbilde = 3
+    } else if (x == 5 && y == 1) {
+        atbilde = "Tuksums";
+    } else if (x == 6 && y == 1) {
+        atbilde = 1
+    } else if (x == 7 && y == 1) {
+        atbilde = 1
+    } else if (x == 8 && y == 1) {
+        atbilde = 1
+    } else if (x == 1 && y == 2){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 2) {
+        atbilde = 3
+    } else if (x == 3 && y == 2) {
+        atbilde = 3
+    } else if (x == 4 && y == 2) {
+        atbilde = 3
+    } else if (x == 5 && y == 2) {
+        atbilde = 6
+    } else if (x == 6 && y == 2) {
+        atbilde = 1
+    } else if (x == 7 && y == 2) {
+        atbilde = 1
+    } else if (x == 8 && y == 2) {
+        atbilde = 1
+    } else if (x == 1 && y == 3){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 3) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 3) {
+        atbilde = 3
+    } else if (x == 4 && y == 3) {
+        atbilde = 2
+    } else if (x == 5 && y == 3) {
+        atbilde = 2
+    } else if (x == 6 && y == 3) {
+        atbilde = 2
+    } else if (x == 7 && y == 3) {
+        atbilde = 2
+    } else if (x == 8 && y == 3) {
+        atbilde = "Tuksums"
+    } else if (x == 1 && y == 4){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 4) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 4) {
+        atbilde = 4
+    } else if (x == 4 && y == 4) {
+        atbilde = 2
+    } else if (x == 5 && y == 4) {
+        atbilde = 4
+    } else if (x == 6 && y == 4) {
+        atbilde = 5
+    } else if (x == 7 && y == 4) {
+        atbilde = "Tuksums"
+    } else if (x == 8 && y == 4) {
+        atbilde = "Tuksums"
+    } else if (x == 1 && y == 5){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 5) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 5) {
+        atbilde = 4
+    } else if (x == 4 && y == 5) {
+        atbilde = 4
+    } else if (x == 5 && y == 5) {
+        atbilde = 4
+    } else if (x == 6 && y == 5) {
+        atbilde = "Tuksums"
+    } else if (x == 7 && y == 5) {
+        atbilde = 6
+    } else if (x == 8 && y == 5) {
+        atbilde = 6
+    } else if (x == 1 && y == 6){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 6) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 6) {
+        atbilde = 4 
+    } else if (x == 4 && y == 6) {
+        atbilde = 4
+    } else if (x == 5 && y == 6) {
+        atbilde = 4
+    } else if (x == 6 && y == 6) {
+        atbilde = "Tuksums"
+    } else if (x == 7 && y == 6) {
+        atbilde = 6
+    } else if (x == 8 && y == 6) {
+        atbilde = 6
+    } else if (x == 1 && y == 7){
+        atbilde = "Tuksums"
+    } else if (x == 2 && y == 7) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 7) {
+        atbilde = 4
+    } else if (x == 4 && y == 7) {
+        atbilde = 4
+    } else if (x == 5 && y == 7) {
+        atbilde = 4
+    } else if (x == 6 && y == 7) {
+        atbilde = "Tuksums"
+    } else if (x == 7 && y == 7) {
+        atbilde = 6
+    } else if (x == 8 && y == 7) {
+        atbilde = 6
+    } else if (x == 1 && y == 8){
+        atbilde = 7
+    } else if (x == 2 && y == 8) {
+        atbilde = "Tuksums"
+    } else if (x == 3 && y == 8) {
+        atbilde = "Tuksums"
+    } else if (x == 4 && y == 8) {
+        atbilde = "Tuksums"
+    } else if (x == 5 && y == 8) {
+        atbilde = "Tuksums"
+    } else if (x == 6 && y == 8) {
+        atbilde = "Tuksums"
+    } else if (x == 7 && y == 8) {
+        atbilde = 6
+    } else if (x == 8 && y == 8) {
+        atbilde = 6
+    } else if (x == 1 && y == 9){
+        atbilde = 7
+    } else if (x == 2 && y == 9) {
+        atbilde = 7
+    } else if (x == 3 && y == 9) {
+        atbilde = "Tuksums"
+    } else if (x == 4 && y == 9) {
+        atbilde = "Tuksums"
+    } else if (x == 5 && y == 9) {
+        atbilde = "Tuksums"
+    } else if (x == 6 && y == 9) {
+        atbilde = 1
+    } else if (x == 7 && y == 9) {
+        atbilde = 1
+    } else if (x == 8 && y == 9) {
+        atbilde = 1
+    } else if (x == 1 && y == 10){
+        atbilde = 7
+    } else if (x == 2 && y == 10) {
+        atbilde = 7
+    } else if (x == 3 && y == 10) {
+        atbilde = 7
+    } else if (x == 4 && y == 10) {
+        atbilde = "Tuksums"
+    } else if (x == 5 && y == 10) {
+        atbilde = "Tuksums"
+    } else if (x == 6 && y == 10) {
+        atbilde = 1
+    } else if (x == 7 && y == 10) {
+        atbilde = 1
+    } else if (x == 8 && y == 10) {
+        atbilde = 1
+    } else {
+        atbilde = "Kluda"
+    }
+    return atbilde;
+}
+
+let answer = Coords(3, 5)
